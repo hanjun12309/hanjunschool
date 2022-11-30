fba8d0d0297bd1c17942788b24ecc4eac65d555f
hanjun12309 <119509025+hanjun12309@users.noreply.github.com>
2022-11-30 23:05:53 +0900

    main.blocks, main.ts 업데이트하기

--- main.ts
+++ main.ts
@@ -1,5 +1,29 @@
 radio.onReceivedNumber(function (receivedNumber) {
-    while (input.buttonIsPressed(Button.AB) && 0 == receivedNumber) {
+    if (receivedNumber == 2 && false) {
+        basic.showIcon(IconNames.Happy)
+    } else if (receivedNumber == 1) {
+        basic.showIcon(IconNames.Sad)
+    }
+})
+input.onButtonPressed(Button.A, function () {
+    호감도 += randint(0, 3)
+    if (10 <= 호감도) {
+        for (let index = 0; index < 2; index++) {
+            basic.showIcon(IconNames.Heart)
+            basic.clearScreen()
+        }
+    } else {
+        for (let index = 0; index < 2; index++) {
+            basic.showIcon(IconNames.SmallHeart)
+            basic.clearScreen()
+        }
+    }
+    radio.sendNumber(1)
+    basic.pause(3000)
+})
+input.onButtonPressed(Button.AB, function () {
+    radio.sendNumber(2)
+    for (let index = 0; index < 4; index++) {
         basic.showLeds(`
             . . . . .
             . . . . .
@@ -29,30 +53,6 @@ radio.onReceivedNumber(function (receivedNumber) {
             . . . . .
             `)
     }
-    if (receivedNumber == 2) {
-    	
-    } else if (receivedNumber == 1) {
-    	
-    }
-})
-input.onButtonPressed(Button.A, function () {
-    호감도 += randint(0, 3)
-    if (10 <= 호감도) {
-        for (let index = 0; index < 2; index++) {
-            basic.showIcon(IconNames.Heart)
-            basic.clearScreen()
-        }
-    } else {
-        for (let index = 0; index < 2; index++) {
-            basic.showIcon(IconNames.SmallHeart)
-            basic.clearScreen()
-        }
-    }
-    radio.sendNumber(1)
-    basic.pause(3000)
-})
-input.onButtonPressed(Button.AB, function () {
-    radio.sendNumber(2)
 })
 input.onButtonPressed(Button.B, function () {
     basic.showString("" + (호감도))
